82eca6a73aa282099b239dba6a3de9b0a5449f7c
Naim <skylike2904@gmail.com>
2022-03-18 03:21:10 +0600

    bug fix

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,11 @@
 const navbarList = document.querySelector(".navbar__list");
 const navbarListContainer = document.querySelector(".navbar__list__container");
 const navbarMobileMenu = document.querySelector(".navbar__mobile__menu");
-const mainSection = document.querySelector("main");
+
 const homeMenu = document.querySelector(".home__page");
 const aboutMenu = document.querySelector(".about__page");
 const projectsMenu = document.querySelector(".projects__page");
 const contactMenu = document.querySelector(".contact__page");
-const navHighlight = document.querySelector(".highligh");
 
 // NAVBAR MOBILE MENU
 navbarMobileMenu.addEventListener("click", () => {
